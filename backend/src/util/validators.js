a7d18ab55c46f1d9f0c8584150dee5a036b97b74
Edson J Vargas <edson.jhair@yahoo.com>
2024-12-22 15:16:03 -0500

    add auth context

--- backend/src/util/validators.js
+++ backend/src/util/validators.js
@@ -11,7 +11,7 @@ export const validateRegisterInput = (
   }
 
   if (email.trim() === '') {
-    errors.mail = 'Email must not be empty';
+    errors.email = 'Email must not be empty';
   } else {
     const regEx =
       /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
